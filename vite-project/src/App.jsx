2527a79abb5768873462498be07681a99bc4d481
Shruti <shrutigalande2@gmail.com>
2024-05-21 22:37:15 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './assets/components/Counter'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Hello from react</h1>
+      <Counter/>
+    </>
+  )
+}
+
+export default App
